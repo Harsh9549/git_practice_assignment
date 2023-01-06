59002c5e7782ff54fe76f7441110a73dcd119041
Harsh <harshkumarsingh9549@gmail.com>
2023-01-06 21:28:04 +0530

    harsh

---
+++ temp.js
@@ -0,0 +1,31 @@
+  let factor=0
+    for(let i=0;i<=num;i++){
+        if(num%i==0){
+        factor++;
+    }
+    }
+    if(factor==2){
+       console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
+    
+}
+ og("No")
+     }let flag= "";
+    for(let i= N-1;i>=0;i--){
+       flag= flag + str[i] 
+    }
+        
+        if(flag == str){
+        console.log("Yes")
+    
+    
+    }
+  
+
+    
+    
+     else {
+         console.l
